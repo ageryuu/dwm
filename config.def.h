10a20b52148934b18e9afc9e42df582b911f8c95
ageryuu <ageryuu@gmail.com>
2022-01-21 21:26:03 +0800

    remove firefox rule

--- config.def.h
+++ config.def.h
@@ -28,7 +28,6 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  "Places",   "Library",  0,       1,           -1 },
 	{ "mpv",      NULL,       NULL,       0,            1,           -1 },
 };
 
